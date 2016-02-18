8acfaa90305091a25f9e35d53fe70f1dc0e0a34d
unknown <jerry.mitchell@GBKTYVL001D0953.kits.kfplc.com>
2016-02-18 13:22:47 +0000

    my first commit

---
+++ mpg.py
@@ -0,0 +1,12 @@
+# miles per gallon calculator 
+litresPerGallon = 4.54609
+
+print('MPG calculator')
+print('==============')
+print('Input mileage')
+mileage = input()
+print('Input litres used')
+litresUsed = input()
+mpg = int(mileage) / (int(litresUsed) / litresPerGallon)
+print('MPG = ' + str(mpg))
+
