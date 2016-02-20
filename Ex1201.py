82df13ac1eb2b676d6035c1a46a96ade1112fc71
Jerry Mitchell <jerryinsomerset@gmail.com>
2016-02-20 18:53:07 +0000

    pi new file

---
+++ Ex1201.py
@@ -0,0 +1,15 @@
+
+print("Please enter your name")
+myName = input()
+print("Please enter your age")
+myAge = int(input())
+
+if myName == "Jerry":
+    print("Hi Jerry")
+elif myAge < 50:
+    print("You are not Jerry")
+elif myAge > 2000:
+    print("Unlike you, Jerry is not an undead immortal vampire")
+elif myAge > 100:
+    print("You could be over 100?")
+
